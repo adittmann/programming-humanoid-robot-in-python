2812b18529996c9a3db6e4ddddc424d3adb583b6
Alexander Dittmann <adittmann@mail.tu-berlin.de>
2015-11-13 15:27:22 +0100

    forward kinematics, finished 1st part

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -36,13 +36,24 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
+        # YOUR CODE HERE
         self.chains = { 'Head': ['HeadYaw', 'HeadPitch'],
 						'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw', 'LHand'],
-						'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'RAnkleRoll'],
-						'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'LAnkleRoll'],
+						'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+						'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
 						'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'RHand'],
-                       # YOUR CODE HERE
                        }
+        self.jointLengths = {'HeadYaw' : (0, 0, 126.50), 'HeadPitch' : (0, 0, 0),
+                             # left arm and leg
+                            'LShoulderPitch' : (0, 98.00, 0), 'LShoulderRoll' : (0, 0, 0), 'LElbowYaw' : (105, 15, 0),
+                            'LElbowRoll' : (0, 0, 0), 'LWristYaw' : (55.95, 0, 0), 'LHand' : (57.75, 0, 12.31),
+                            'LHipYawPitch' : (0, 50.00, -85.00), 'LHipRoll' : (0, 0, 0), 'LHipPitch' : (0, 0, 0), 
+                             'LKneePitch', : (0, 0, -100.00) 'LAnklePitch' : (0, 0, -102.90), 'LAnkleRoll' : (0, 0, 45.19),
+                             # right arm and leg
+                            'RShoulderPitch' : (0, -98.00, 0), 'RShoulderRoll' : (0, 0, 0), 'RElbowYaw' : (105, -15, 0),
+                            'RElbowRoll' : (0, 0, 0), 'RWristYaw' : (55.95, 0, 0), 'RHand' : (57.75, 0, 12.31),
+                            'RHipYawPitch' : (0, -50.00, -85.00), 'RHipRoll' : (0, 0, 0), 'RHipPitch' : (0, 0, 0), 
+                             'RKneePitch', : (0, 0, -100.00) 'RAnklePitch' : (0, 0, -102.90), 'RAnkleRoll' : (0, 0, 45.19)}
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -66,7 +77,10 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         Ry = matrix('c 0 s 0; 0 1 0 0; -s 0 c 0 ; 0 0 0 1')
         Rz = matrix('c -s 0 0; s c 0 0; 0 0 1 0 ; 0 0 0 1')
 
-        T += np.dot(np.dot(Rx,Ry)Rz)
+        T = np.dot(np.dot(Rx,Ry)Rz)
+        T[3][0] = self.jointLengths[joint_name][0]
+        T[3][1] = self.jointLengths[joint_name][1]
+        T[3][2] = self.jointLengths[joint_name][2]
 
 
         return T
